f6ce134df40a03f4f158a0d8f5f4401caf8a801c
zhenhua.zhu <zhenhua.zhu@aqrose.com>
2026-05-22 14:05:53 +0800

    Add WorkTree button prompt

--- WPFDemo/MainWindow.xaml.cs
+++ WPFDemo/MainWindow.xaml.cs
@@ -25,5 +25,10 @@ namespace WPFDemo
         {
             MessageBox.Show("Hello");
         }
+
+        private void WorkTreeButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("我是WorkTree按钮");
+        }
     }
 }
